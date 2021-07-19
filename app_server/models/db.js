cf16108acaaad803c91f37d16cf3a9e4b10225f9
mlinsenbigler <JrBlaze2001@hotmail.com>
2021-07-19 18:02:46 -0400

    Config mongoose connection and seeded data

---
+++ app_server/models/db.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1';
+const dbURI = 'mongodb://localhost:27017/travlr';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
+}
+const readLine = require('readline');
+
+mongoose.set('useUnifiedTopology', true);
+
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+// For nodemon restarts                                 
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+require('./travlr');
