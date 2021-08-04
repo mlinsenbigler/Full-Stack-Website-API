5e341f3f5d61850d46437dd525fea74c95be8caf
mlinsenbigler <JrBlaze2001@hotmail.com>
2021-08-04 17:11:38 -0400

    Added Bootstrap

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,32 @@
+export const trips = 
+[    {
+        "code": "GALR210214",
+        "name": "Gale Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-02-14T08:00:00Z",
+        "resort": "Emerald Bay, 3 stars",
+        "perPerson": "799.00",
+        "image": "reef1.jpg",
+        "description": "At Gale... Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis. </p> <p> Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet."
+    },
+    {
+        "code": "DAWR210315",
+        "name": "Dawson's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-03-15T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1199.00",
+        "image": "reef2.jpg",
+        "description": "At Dawson's... Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis. </p> <p> Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet."
+    },
+    {
+        "code": "CLAR210621",
+        "name": "Claire's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-06-21T08:00:00Z",
+        "resort": "Coral Sands, 5 stars",
+        "perPerson": "1999.00",
+        "image": "reef3.jpg",
+        "description": "At Claire's... Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis. </p> <p> Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet."
+    }
+]
